2e29b8e9443083f5e2aceed1f4cea0cb32c20590
leonluong1 <leon1838luong@yahoo.com>
2023-09-22 10:50:05 -0700

    Add comments

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,9 +1,11 @@
 import os
 import csv
 
+# Importing csv file, must be in PyPoll folder.
 bankPath = os.path.join(".", "Resources", "budget_data.csv")
 
 def analyze_records():
+    # Opening csv file
     with open(bankPath, "r") as bankFile:
         monthTotal = 0
         netProfit = 0
@@ -12,18 +14,28 @@ def analyze_records():
         maxIncDate = ""
         maxDec = 0
         maxDecDate = ""
+        prevAmount = 0
+
+        # Creating reader object and storing headers
         csvreader = csv.reader(bankFile, delimiter=",")
         headers = next(csvreader)
-        prevAmount = 0
+        
         for row in csvreader:
             value = int(row[1])
             monthTotal += 1
+
+            # Adding all profits and losses together to find net profit
             netProfit += value
+
+            # Finding the current profits/losses this month by finding the difference from previous month
             currChange = value - prevAmount
+
+            # Adding all the monthly changes to find average later
             if monthTotal > 1:
                 totalChange += currChange
             prevAmount = value
 
+            # Finding the greatest monthly increase/decrease while iterating through data
             if currChange > maxInc:
                 maxInc = currChange
                 maxIncDate = row[0]
@@ -31,6 +43,7 @@ def analyze_records():
                 maxDec = currChange
                 maxDecDate = row[0]
 
+        # Printing report to terminal
         print("\nFinancial Analysis\n")
         print("----------------------------\n")
         print(f"Total Months: {monthTotal}")
@@ -39,6 +52,7 @@ def analyze_records():
         print(f"Greatest Increase in Profits: {maxIncDate} (${maxInc})")
         print(f"Greatest Decrease in Profits: {maxDecDate} (${maxDec})")
 
+        # Writing report to "./analysis/analysis.txt"
         outputPath = os.path.join(".", "analysis", "analysis.txt")
         outputFile = open(outputPath, "w")
         outputFile.write("Financial Analysis\n\n")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,22 +1,28 @@
 import os
 import csv
 
+# Importing csv file, must be in PyPoll folder.
 electionPath = os.path.join(".", "Resources", "election_data.csv")
 
 def analyze_election():
+    # Opening csv file
     with open(electionPath, "r") as electionFile:
         voteTotal = 0
         candidates = []
         voteDict = {}
+
+        # Creating reader object and storing headers
         csvreader = csv.reader(electionFile, delimiter=",")
         headers = next(csvreader)
         for row in csvreader:
+            name = row[2]
             voteTotal += 1
 
-            name = row[2]
+            # Appending each unique candidate to a list
             if name not in candidates:
                 candidates.append(name)
 
+            # Adding votes for each candidate to a dict
             if name in voteDict.keys():
                 voteDict[name][0] += 1
             else:
@@ -24,12 +30,15 @@ def analyze_election():
 
         winner = ""
         winnerTotal = 0
+
+        # Iterating through vote dict to find candidate with highest vote total (winner).
         for name in candidates:
             voteDict[name].append(round((voteDict[name][0]/voteTotal)*100, 3))
             if voteDict[name][0] > winnerTotal:
                 winner = name
                 winnerTotal = voteDict[name][0]
 
+        # Printing report to terminal
         print("\nElection Results\n")
         print("----------------------------\n")
         print(f"Total Votes: {voteTotal}\n")
@@ -41,6 +50,7 @@ def analyze_election():
         print(f"Winner: {winner}\n")
         print("----------------------------\n")
 
+        # Writing report to "./analysis/analysis.txt"
         outputPath = os.path.join(".", "analysis", "analysis.txt")
         outputFile = open(outputPath, "w")
         outputFile.write("Election Results\n")
